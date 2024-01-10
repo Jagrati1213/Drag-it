ee211192e39a618ba1ef6b07fa351df76adb975e
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-10 18:26:53 +0530

    JG: import flow component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Flow from './components/Flow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body" style={{ background: 'whitesmoke', height: '100vh', width: '100%', overflowY: 'scroll' }}>
+        <h1 style={{ textAlign: 'center' }}>React Flow</h1>
+        <Flow />
+      </div>
     </div>
   );
 }
