bcf230951574cb5c8cc612317282326ad7189a81
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-11 18:44:29 +0530

    JG: add layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Flow from './components/Flow';
+import { Layout } from 'antd';
+import HeaderComponent from './components/Header';
+import SiderComponent from './components/SiderComponent';
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <div className="body" style={{ background: 'whitesmoke', height: '100vh', width: '100%', overflowY: 'scroll' }}>
-        <Flow />
-      </div>
-    </div>
+
+    <Layout className='App'>
+      <HeaderComponent />
+      <Layout>
+        <SiderComponent />
+        <Content style={{ background: 'whitesmoke', minHeight: '90vh', maxHeight: '90vh', width: '100%' }}>
+          <Flow />
+        </Content>
+      </Layout>
+      {/* <Footer>Footer</Footer> */}
+    </Layout >
   );
 }
 
