cd98b6da764bec0ff8ab33352a46a39b256b2867
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-11 16:06:32 +0530

    JG: remove heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <div className="body" style={{ background: 'whitesmoke', height: '100vh', width: '100%', overflowY: 'scroll' }}>
-        <h1 style={{ textAlign: 'center' }}>React Flow</h1>
         <Flow />
       </div>
     </div>
