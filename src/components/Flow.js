6b6554b1e34e3ddebfc45f4830ecbdaaaf44bd6d
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-12 12:42:01 +0530

    JG: add constant node and edges

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -14,45 +14,10 @@ import ReactFlow,
 import 'reactflow/dist/style.css';
 import TextUpdaterNode from "./TextUpdater";
 import '../index.css';
+import { InitialEdges, InitialNodes } from "../utils/Constant";
 
-// collection of nodes
-const initialNodes = [
-    // object of node --> id, position(required)
-    {
-        id: '1',
-        position: { x: 500, y: 200 },
-        data: { label: 'Node 1' },
-        type: 'input',
-        style: { backgroundColor: '#6ede87', color: 'white' },
-    },
-    {
-        id: '2',
-        type: 'textUpdater',
-        position: { x: 200, y: 300 },
-        data: { value: 123 }
-    },
 
-    {
-        id: '3',
-        position: { x: 700, y: 300 },
-        data: { label: 'Node 3' },
-        style: { backgroundColor: '#ff0072', color: 'white' },
-    },
-    {
-        id: '4',
-        type: 'output',
-        position: { x: 700, y: 500 },
-        data: { label: 'Node 4' },
-        style: { backgroundColor: '#6865A5', color: 'white' },
-    }
-];
-
-// Collection of Edges
-const initialEdges = [
-    { id: '1-2', source: '1', target: '2', label: 'User', type: 'step' },
-    { id: '1-3', source: '1', target: '3', label: 'Admin', type: 'step', animated: true },
-];
-
+// Node colors
 const nodeColor = (node) => {
     switch (node.type) {
         case 'input':
@@ -69,8 +34,8 @@ const nodeTypes = { textUpdater: TextUpdaterNode };
 
 const Flow = () => {
 
-    const [nodes, setNodes] = useState(initialNodes);
-    const [edges, setEdges] = useState(initialEdges);
+    const [nodes, setNodes] = useState(InitialNodes);
+    const [edges, setEdges] = useState(InitialEdges);
 
     // Make dregAble nodes
     const onNodesChangeHandler = useCallback(
@@ -87,17 +52,24 @@ const Flow = () => {
         (params) => { setEdges((edges) => addEdge({ ...params, animated: false }, edges)) },
         []);
 
-    return <ReactFlow nodes={nodes} onNodesChange={onNodesChangeHandler} edges={edges} onEdgesChange={onEdgesChangeHandler} onConnect={onConnectHandler} fitView nodeTypes={nodeTypes}>
-        <Background color="#ccc" variant="lines" />
-        <Panel style={{ fontSize: '1.5rem', marginBottom: '2rem' }}>
-            <h2>React Flow</h2>
-        </Panel>
-        <Controls>
-            <ControlButton onClick={() => alert(`check out my GitHub profile 🚀 : https://github.com/Jagrati1213`)}>
-                <GithubFilled />
-            </ControlButton>
-        </Controls>
-        <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} />
-    </ReactFlow>
+    return (
+        <ReactFlow
+            nodes={nodes}
+            onNodesChange={onNodesChangeHandler}
+            edges={edges}
+            onEdgesChange={onEdgesChangeHandler}
+            onConnect={onConnectHandler}
+            fitView
+            nodeTypes={nodeTypes}
+        >
+
+            <Background color="#ccc" variant="lines" />
+            <Controls showInteractive={false} className="controls-btn">
+                <ControlButton onClick={() => alert(`check out my GitHub profile 🚀 : https://github.com/Jagrati1213`)}>
+                    <GithubFilled />
+                </ControlButton>
+            </Controls>
+            <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} />
+        </ReactFlow>)
 }
 export default Flow;
